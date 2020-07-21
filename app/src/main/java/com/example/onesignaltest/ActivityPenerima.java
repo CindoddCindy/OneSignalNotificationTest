bfcd18d9dec9f7ee97b15d166639e98b72ea5a7b
CindoddCindy <cindodcindy@gmail.com>
2020-07-21 11:09:34 +0700

    firebasenya ngambek

---
+++ app/src/main/java/com/example/onesignaltest/ActivityPenerima.java
@@ -0,0 +1,14 @@
+package com.example.onesignaltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPenerima extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penerima);
+    }
+}
---
+++ app/src/main/java/com/example/onesignaltest/PenerimaActivity.java
@@ -0,0 +1,4 @@
+package com.example.onesignaltest;
+
+public class PenerimaActivity {
+}
